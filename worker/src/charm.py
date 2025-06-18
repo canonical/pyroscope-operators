4420ae118eb40f9d0dec054929ed0bca5c425dca
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-06-18 11:20:34 +0200

    added charm tracing to worker

--- worker/src/charm.py
+++ worker/src/charm.py
@@ -5,8 +5,11 @@
 """Charmed Operator for Pyroscope; a lightweight object storage based profiling backend."""
 
 import logging
+from pathlib import Path
 
+import ops_tracing
 from ops.charm import CharmBase
+
 from pyroscope import PyroscopeWorker
 
 logger = logging.getLogger(__name__)
@@ -15,10 +18,19 @@ logger = logging.getLogger(__name__)
 class PyroscopeWorkerCharm(CharmBase):
     """Charmed Operator for Pyroscope; a distributed profiling backend."""
 
-
     def __init__(self, *args):
         super().__init__(*args)
         self.worker = PyroscopeWorker(self)
+        self._setup_charm_tracing()
+
+    def _setup_charm_tracing(self):
+        # we can't use ops.tracing.Tracing as this charm doesn't integrate with certs/tracing directly,
+        # but the data goes through the coordinator.
+        if self.worker.is_ready():
+            charm_tracing_endpoint, charm_tracing_ca_path = self.worker.charm_tracing_config()
+            if charm_tracing_endpoint:
+                ca_text = Path(charm_tracing_ca_path).read_text() if charm_tracing_ca_path else None
+                ops_tracing.set_destination(charm_tracing_endpoint+"/v1/traces", ca_text)
 
 
 if __name__ == "__main__":  # pragma: nocover
