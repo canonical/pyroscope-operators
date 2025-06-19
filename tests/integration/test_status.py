194c7736ce79a157eb222d64d75e819be373fa3e
michael <michael.dmitry98@gmail.com>
2025-06-19 16:16:54 +0300

    catch RetryError

--- tests/integration/test_status.py
+++ tests/integration/test_status.py
@@ -4,6 +4,7 @@
 # See LICENSE file for licensing details.
 
 import pytest
+from tenacity import RetryError
 import jubilant
 from jubilant import Juju, all_blocked
 
@@ -36,16 +37,20 @@ def test_pyroscope_block_if_worker_relations_depart(juju: Juju, monolithic):
     workers = (WORKER_APP, ) if monolithic else ALL_WORKERS
     for worker in workers:
         juju.remove_relation(worker, PYROSCOPE_APP)
-    # THEN pyroscope coordinator stays in blocked state
-    juju.wait(lambda status: jubilant.all_blocked(status, PYROSCOPE_APP),
-              timeout=1000)
+    # THEN pyroscope coordinator and all workers stay in blocked state
+    juju.wait(
+            lambda status: jubilant.all_blocked(status, PYROSCOPE_APP, *workers),
+            timeout=1000,
+            delay=5,
+            successes=6,
+        )
 
 def test_profiles_ingestion_fails(juju: Juju):
     # GIVEN a pyroscope cluster with no s3 integrator and no workers
     # WHEN we emit a profile through Pyroscope's HTTP API server
     hostname = get_unit_ip_address(juju, PYROSCOPE_APP, 0)
     # THEN we're unable to emit profiles
-    with pytest.raises(AssertionError):
+    with pytest.raises(RetryError):
         emit_profile(f"{hostname}:{_nginx_port}")
 
 def test_profiles_query_fails(juju: Juju):
@@ -54,6 +59,6 @@ def test_profiles_query_fails(juju: Juju):
     hostname = get_unit_ip_address(juju, PYROSCOPE_APP, 0)
     # THEN we fail to query any profile
     # AND we get an empty list of samples
-    with pytest.raises(AssertionError):
+    with pytest.raises(RetryError):
         get_profiles_patiently(f"{hostname}:{_nginx_port}")
 
