469f6cc838f42c10cb9f5cc0de5859ada00611a8
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-08-22 11:13:38 +0200

    trfk

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -256,4 +256,4 @@ def _deploy_and_configure_minio(juju: Juju):
 
 
 def get_ingress_proxied_hostname(juju: Juju):
-    return json.loads(juju.run(TRAEFIK_APP+"/0", 'show-proxied-endpoints').results['proxied-endpoints'])['traefik']['url']
+    return json.loads(juju.run(TRAEFIK_APP+"/0", 'show-proxied-endpoints').results['proxied-endpoints'])[TRAEFIK_APP]['url']
